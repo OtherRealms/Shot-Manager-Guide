6e900e92791992cf575b95d838767665dacca7a1
pablo_tochez <tochezpab@gmail.com>
2023-05-07 20:02:58 +1000

    fix

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
 
-extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinxcontrib.programoutput"]
+extensions = ["sphinx.ext.autodoc", "sphinxcontrib.programoutput"]
 
 
